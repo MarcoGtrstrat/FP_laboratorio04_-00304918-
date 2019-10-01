fbf9aa71a1cd52deb90b97509d5f43af6f79a3aa
MarcoGtrstrat <00304918@uca.edu.sv>
2019-09-30 21:31:40 -0600

    Segundo commit

---
+++ Ejercicio4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(){
+	
+	//Programa que determina si un número es par o impar
+ 
+  int num;
+ 
+  cout<<"Escriba un numero: ";
+  cin>>num;
+ 
+  if(num % 2 == 0){
+        cout<<"El numero es Par\n";
+        }
+ 
+   else{
+     cout<<"El numero es Impar\n";
+     }
+     return 0;
+}
